2064696e37e8293a80befd2b6330bb5dbabe4a76
Luke Freedman <lfreedma@fandm.edu>
2023-04-04 13:33:10 -0400

    Synced work on LoginActivity and Main

--- app/src/main/java/dill/group/riparianreport/Main.java
+++ app/src/main/java/dill/group/riparianreport/Main.java
@@ -7,19 +7,54 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Main extends AppCompatActivity {
 
     // Test to see if I can add to the code (Luke)
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         Button reportButton = findViewById(R.id.report_button);
         reportButton.setOnClickListener(view -> handleReportButton());
+
+        Button login_button = findViewById(R.id.goto_login);
+
+        TextView tv = findViewById(R.id.disp_username);
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            tv.setText(user.getEmail());
+        }
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        //if (user == null) {
+            Intent i = new Intent(this,LoginActivity.class);
+            startActivity(i);
+        //}
     }
 
     public void handleReportButton() { // Makes a new "Report" (Form)
