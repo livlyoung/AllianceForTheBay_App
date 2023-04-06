3e6447d132b2b8da9a5351d226fa4a9d196751ae
Luke Freedman <lfreedma@fandm.edu>
2023-04-06 12:02:34 -0400

    Created SplashScreen and its layout file.
    After the timer is up, gets the user:
    If the user returns null, opens LoginActivity
    If the user is defined, opens Main

---
+++ app/src/main/java/dill/group/riparianreport/SplashScreen.java
@@ -0,0 +1,59 @@
+package dill.group.riparianreport;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashScreen extends AppCompatActivity {
+
+    SharedPreferences prefs = null;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
+
+        View view = findViewById(R.id.splash_screen);
+
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user == null) {
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }
+                else {
+                    //Toast.makeText(getApplicationContext(),user.getEmail(),Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), Main.class));
+                }
+
+            }
+        }, 1375);
+
+        prefs = getSharedPreferences("dill.group.riparianreport", MODE_PRIVATE);
+
+    }
+}
