a9cba52d37a4b446f93232f983ac8442f86dfb61
Luke Freedman <lfreedma@fandm.edu>
2023-04-25 01:17:26 -0400

    Partially finished onMarkerClick implementation. Currently, instead of initializing a Report and displaying it, the map outputs the contents of the Report to Logcat. Will have to update soon!

--- app/src/main/java/dill/group/riparianreport/Map.java
+++ app/src/main/java/dill/group/riparianreport/Map.java
@@ -30,6 +30,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -41,6 +43,18 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
     @Override
     public boolean onMarkerClick(@NonNull Marker marker) {
         // Upon clicking a specific marker, it will get the most recent report that was submitted from that location.
+        String title = marker.getTitle();
+        HashMap<String,String> qa = new HashMap<String, String>();
+        for (HashMap<String, String> curr : Main.reports) {
+            if (Objects.equals(curr.get("Site name or location"), title)) {
+                for (String s : curr.keySet()) {
+                    Log.d("DEBUG", s + " : " + curr.get(s));
+                    // TODO: Ask the others how to initialize a Report using this information
+                    // TODO: (Currently outputs to Logcat instead)
+                }
+                qa = curr;
+            }
+        }
         return false;
     }
 
@@ -59,7 +73,7 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
 
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
-            StringBuffer data = new StringBuffer();
+            StringBuilder data = new StringBuilder();
             String curr;
             while ((curr = in.readLine()) != null) {
                 data.append(curr);
@@ -70,14 +84,11 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
             JSONObject j_location = geometry.getJSONObject("location");
             latlng = new LatLng(j_location.getDouble("lat"),j_location.getDouble("lng"));
         }
-        catch (MalformedURLException murle) {
+        catch (MalformedURLException | JSONException murle) {
             murle.printStackTrace();
         }
         catch (IOException ioe) {
             ioe.printStackTrace();
-        }
-        catch (JSONException jsone) {
-            jsone.printStackTrace();
         }
         return latlng;
     }
@@ -90,9 +101,15 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
                     if (output != null) {
                         Random rng = new Random();
                         site_latlng.add(output);
-
-                        googleMap.addMarker(new MarkerOptions().position(output).title(title).icon(BitmapDescriptorFactory.defaultMarker((float)60.0 + rng.nextFloat()*(float)60.0)).alpha(0.7f));
+                        MarkerOptions markOpt = new MarkerOptions().position(output).title(title).icon(BitmapDescriptorFactory.defaultMarker((float)60.0 + rng.nextFloat()*(float)60.0)).alpha(0.3f);
+                        for (HashMap<String, String> qa : Main.reports) {
+                            if (Objects.equals(qa.get("Site name or location"), title)) {
+                                markOpt.alpha(0.9f);
+                            }
+                        }
+                        googleMap.addMarker(markOpt);
                         googleMap.moveCamera(CameraUpdateFactory.newLatLng(output));
+                        googleMap.moveCamera(CameraUpdateFactory.zoomTo(10f));
                     }
                 }
             });
@@ -117,19 +134,6 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
     String[] site_locations = {"232 S Bridge St, Christiana, PA 17509", "232 Old Dam Rd, Christiana PA", "301 E Newport Rd, Lititz, PA 17543", "1950 Apostle Way, Lancaster, PA 17603", "400 Becker Rd, Leola PA 17540", "825 Lititz Rd, Manheim, PA 17545, USA", "213 S Hartman St, Manchester, PA 17345", "747 Kiner Ave, Lebanon PA", "804 Stone House Ln, Lititz, PA", "20 Wildflower Lane, Lancaster PA 17603", "388 School Lane Rd, Gap, PA 17527", "1093 Mt Pleasant Rd, Quarryville, PA 17566", "36 Maple Shade Rd, Christiana, PA", "755 Summit Dr, Lancaster, PA 17601, USA", "234 S Groffdale Rd, Leola, PA 17540, USA", "85 Spring Run Road Conestoga, PA", "3060 Harrisburg Pike, Landisville, PA 17538", "40.086681, -76.391330", "1236 Woolen Mill Rd, Stewartstown, PA 17363, USA", "1753 Mill Rd, Elizabethtown PA 17022", "423 Valley Road, Goldsboro, PA", "1657 PA-116, Spring Grove, PA 17362", "1036 Prawls Hollow Rd. Peach Bottom, PA 17563", "475 Buch Ave, Lancaster PA 17601", "1987 Glen Rock Rd. Glen Rock, PA 17327", "136 Voneida St, Narvon, PA 17555, USA", "125 Hopeland Rd. Newmanstown, PA", "450 W Locust St, Oxford, PA 19363", "220 Distillery Rd, Newmanstown, PA 17073, USA", "737 E Millport Rd, Lititz, PA 17543 (40.134484, -76.258895)", "2 Township Dr, Paradise, PA 17562 (40.0048818002664, -76.10987308958515)", "430 Quaker Church Rd, York Springs PA", "153 Millway Rd, Ephrata, PA 17522", "5608 Meadville Rd, Gap PA", "950 Church Street, Landisville, PA", "300 Quarry Rd, Chambersburg, PA 17202. 39.920293, -77.628496", "263 Byerland Church Rd Willow Street, PA", "40.08012, -76.32258", "416 Lebanon Road, Manheim, PA", "1003 Mondale Rd. Bird in Hand PA 17505", "213 S Hartman St, Manchester, PA 17345 (40.057676, -76.714284)", "80 Logan Rd, Dillsburg, PA 17019", "1236 Woolen Mill Rd, Stewartstown, PA 17363", "208 Kennedy Rd, Airville PA 17302", "1085 Milton Grove Rd, Mt. Joy PA 17552", "34 N. Peiffer Rd Wellsville, PA 17365", "98 Airport Rd, Hanover PA 17331", "2392 Mount Joy Rd, Manheim, PA 17545", "2890 Admire Rd, York PA 17315", "254 Big Oak Rd. Spring Mills", "8 Woodlyn Ct, Lancaster, PA 17602", "Windemere Lane, Landisville, Pennsylvania", "300 Chestnut Street Mount Wolf PA"};
     ArrayList<LatLng> site_latlng = new ArrayList<LatLng>();
 
-    LatLng findClosest(ArrayList<LatLng> lat_lng_list, LatLng location) {
-        double pb = -32767;
-        LatLng lb = new LatLng(0,0);
-        for (LatLng curr : lat_lng_list) {
-            double curr_dist = Math.sqrt(Math.pow((curr.latitude - location.latitude),2) + Math.pow((curr.longitude - location.longitude),2));
-            if (pb < curr_dist) {
-                pb = curr_dist;
-                lb = curr;
-            }
-        }
-        return lb;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -140,18 +144,17 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
         // Get the SupportMapFragment and request notification when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_view);
-        mapFragment.getMapAsync(this);
-
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
         for (int i = 0; i < site_names.length; i++) {
             Map.SiteLatLngExecutor slle = new Map.SiteLatLngExecutor();
             slle.fetch(site_locations[i],site_names[i],googleMap,sllc);
         }
+        googleMap.setOnMarkerClickListener(this);
     }
 }
