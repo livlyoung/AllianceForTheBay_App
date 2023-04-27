0addf538543bd9816bc71b6ba07812d44b3f5a8e
Luke Freedman <lfreedma@fandm.edu>
2023-04-27 13:59:55 -0400

    Made some final changes to the Map Activity (it should now properly read names and locations from the Google Sheet). If there's something that causes errors, let me know and I'll look into fixing it. Otherwise, this is probably the last submission for me. This has been a lot of fun!

--- app/src/main/java/dill/group/riparianreport/Map.java
+++ app/src/main/java/dill/group/riparianreport/Map.java
@@ -144,8 +144,6 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
         }
     }
 
-    String[] site_names = {"Christiana Park", "Stephan Petersheim", "Warwick Twp Saylor Park", "Church of the Apostles", "Hunter Hess", "Shawn Peirson", "Shallow Brook Elementary", "South Lebanon Twp", "Laleh Baublitz", "Usdin", "David Stoltzfus", "Michael King", "Tom Savage", "Summit Living", "Joe Shirk/Davis Stoltzfoos", "Scott Weber", "Fleishmann et al (Swarr Run)", "Hempfield Twp", "Tim White", "Wittel Farm", "Dan McAllister", "Little Creek Park", "Jacob Kaufman", "Ashley Conrad", "Shannan Donivan", "Ron Funk", "Houser", "Oxford Boro", "Tisha Walmer", "Millport Conservancy", "Paradise Twp", "Tom Kessler", "Brilyn Acres (Lynette and Brian Sauder)", "John Stoltzfus", "Hempfield Rec Center", "New Enterprise Stone and Lime", "Abner Stoltzfus", "Overlook Park", "Jason Hondru", "Joseph Glick ", "Shallowbrook Elementary", "Logan Park", "Tim White - TU Planting", "Scott Mullins", "Jackson Edwards", "Jeff and Tina Gleim", "Plum Creek Park", "LCBC Manheim", "Jason Fauth", "John Stoltzfus", "Conestoga Reserve ", "Windemere Lane", "Northeastern Middle School"};
-    String[] site_locations = {"232 S Bridge St, Christiana, PA 17509", "232 Old Dam Rd, Christiana PA", "301 E Newport Rd, Lititz, PA 17543", "1950 Apostle Way, Lancaster, PA 17603", "400 Becker Rd, Leola PA 17540", "825 Lititz Rd, Manheim, PA 17545, USA", "213 S Hartman St, Manchester, PA 17345", "747 Kiner Ave, Lebanon PA", "804 Stone House Ln, Lititz, PA", "20 Wildflower Lane, Lancaster PA 17603", "388 School Lane Rd, Gap, PA 17527", "1093 Mt Pleasant Rd, Quarryville, PA 17566", "36 Maple Shade Rd, Christiana, PA", "755 Summit Dr, Lancaster, PA 17601, USA", "234 S Groffdale Rd, Leola, PA 17540, USA", "85 Spring Run Road Conestoga, PA", "3060 Harrisburg Pike, Landisville, PA 17538", "40.086681, -76.391330", "1236 Woolen Mill Rd, Stewartstown, PA 17363, USA", "1753 Mill Rd, Elizabethtown PA 17022", "423 Valley Road, Goldsboro, PA", "1657 PA-116, Spring Grove, PA 17362", "1036 Prawls Hollow Rd. Peach Bottom, PA 17563", "475 Buch Ave, Lancaster PA 17601", "1987 Glen Rock Rd. Glen Rock, PA 17327", "136 Voneida St, Narvon, PA 17555, USA", "125 Hopeland Rd. Newmanstown, PA", "450 W Locust St, Oxford, PA 19363", "220 Distillery Rd, Newmanstown, PA 17073, USA", "737 E Millport Rd, Lititz, PA 17543 (40.134484, -76.258895)", "2 Township Dr, Paradise, PA 17562 (40.0048818002664, -76.10987308958515)", "430 Quaker Church Rd, York Springs PA", "153 Millway Rd, Ephrata, PA 17522", "5608 Meadville Rd, Gap PA", "950 Church Street, Landisville, PA", "300 Quarry Rd, Chambersburg, PA 17202. 39.920293, -77.628496", "263 Byerland Church Rd Willow Street, PA", "40.08012, -76.32258", "416 Lebanon Road, Manheim, PA", "1003 Mondale Rd. Bird in Hand PA 17505", "213 S Hartman St, Manchester, PA 17345 (40.057676, -76.714284)", "80 Logan Rd, Dillsburg, PA 17019", "1236 Woolen Mill Rd, Stewartstown, PA 17363", "208 Kennedy Rd, Airville PA 17302", "1085 Milton Grove Rd, Mt. Joy PA 17552", "34 N. Peiffer Rd Wellsville, PA 17365", "98 Airport Rd, Hanover PA 17331", "2392 Mount Joy Rd, Manheim, PA 17545", "2890 Admire Rd, York PA 17315", "254 Big Oak Rd. Spring Mills", "8 Woodlyn Ct, Lancaster, PA 17602", "Windemere Lane, Landisville, Pennsylvania", "300 Chestnut Street Mount Wolf PA"};
     ArrayList<LatLng> site_latlng = new ArrayList<LatLng>();
 
     @Override
@@ -165,9 +163,9 @@ public class Map extends AppCompatActivity implements GoogleMap.OnMarkerClickLis
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        for (int i = 0; i < site_names.length; i++) {
+        for (int i = 0; i < Main.names.size(); i++) {
             Map.SiteLatLngExecutor slle = new Map.SiteLatLngExecutor();
-            slle.fetch(site_locations[i],site_names[i],googleMap,sllc);
+            slle.fetch(Main.locations.get(i),Main.names.get(i),googleMap,sllc);
         }
         googleMap.setOnMarkerClickListener(this);
     }
